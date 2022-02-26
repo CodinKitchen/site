fa21587093d74e93b93cc310eb983457d5622212
Gauthier A <gauthier.andre@protonmail.com>
2022-02-27 00:56:58 +0100

    home page design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   content: ["./templates/**/*.twig"],
   theme: {
     extend: {},
+    fontFamily: {
+      'regular': ["SpaceMono"],
+      'bold': ['SpaceMono-Bold'],
+      'italic': ['SpaceMono-Italic'],
+      'bold-italic': ['SpaceMono-BoldItalic'],
+    }
   },
   plugins: [],
 }
