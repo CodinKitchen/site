6a2caa6a7e688489f3efdfe3147312c8754899dd
Gauthier A <gauthier.andre@protonmail.com>
2022-04-26 19:36:50 +0200

    get recordings

--- src/Service/Meeting/MeetingService.php
+++ src/Service/Meeting/MeetingService.php
@@ -3,28 +3,44 @@
 namespace App\Service\Meeting;
 
 use App\Entity\Meeting;
+use App\Repository\MeetingRepository;
 use BigBlueButton\BigBlueButton;
 use BigBlueButton\Core\MeetingLayout;
 use BigBlueButton\Parameters\CreateMeetingParameters;
 use BigBlueButton\Parameters\GetMeetingInfoParameters;
+use BigBlueButton\Parameters\GetRecordingsParameters;
 use BigBlueButton\Parameters\JoinMeetingParameters;
 use BigBlueButton\Responses\CreateMeetingResponse;
+use Symfony\Component\Asset\Packages;
+use Symfony\Component\HttpFoundation\UrlHelper;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Security;
 
 class MeetingService
 {
-    public function __construct(private BigBlueButton $bigBlueButton, private Security $security)
-    {
-    }
+    public function __construct(
+        private BigBlueButton $bigBlueButton,
+        private Security $security,
+        private Packages $packages,
+        private UrlHelper $urlHelper,
+        private UrlGeneratorInterface $urlGenerator,
+        private MeetingRepository $meetingRepository,
+    ) {}
 
     public function join(Meeting $meeting, bool $moderator = false): string
     {
+        $logoUrl = $this->urlHelper->getAbsoluteUrl($this->packages->getUrl('build/images/logo_codinkitchen.png'));
+        $logoutUrl = $this->urlGenerator->generate('home', [], UrlGeneratorInterface::ABSOLUTE_URL);
         $createMeetingParams = new CreateMeetingParameters((string) $meeting->getAttendee()->getBbbMeetingId(), sprintf('Meeting %d', $meeting->getId()));
         $createMeetingParams->setAllowModsToEjectCameras(true);
         $createMeetingParams->setBreakoutRoomsEnabled(false);
         $createMeetingParams->setBreakoutRoomsRecord(false);
         $createMeetingParams->setBreakoutRoomsPrivateChatEnabled(false);
         $createMeetingParams->setMeetingLayout(MeetingLayout::SMART_LAYOUT);
+        $createMeetingParams->setRecord(true);
+        $createMeetingParams->setAllowStartStopRecording(true);
+        $createMeetingParams->setLogoutUrl($logoutUrl);
+        $createMeetingParams->setLogo($logoUrl);
         $meetingResponse = $this->bigBlueButton->createMeeting($createMeetingParams);
 
         $password = $moderator ? $meetingResponse->getModeratorPassword() : $meetingResponse->getAttendeePassword();
@@ -33,12 +49,20 @@ class MeetingService
         if ($meetingResponse->getReturnCode() === CreateMeetingResponse::FAILED) {
             $meetingInfoParams = new GetMeetingInfoParameters((string) $meeting->getAttendee()->getBbbMeetingId(), '');
             $meetingResponse = $this->bigBlueButton->getMeetingInfo($meetingInfoParams);
-            $password = (string) ($moderator ? $meetingResponse->getRawXml()->moderatorPW : $meetingResponse->getRawXml()->attendeePW);
+            $password = (string) ($moderator ? $meetingResponse->getMeeting()->getModeratorPassword() : $meetingResponse->getMeeting()->getAttendeePassword());
         }
 
         $joinMeetingParams = new JoinMeetingParameters((string) $meeting->getAttendee()->getBbbMeetingId(), $firstname, $password);
         $joinMeetingParams->setRedirect(true);
 
         return $this->bigBlueButton->getJoinMeetingURL($joinMeetingParams);
+    }
+
+    public function getRecordings()
+    {
+        $meeting = $this->meetingRepository->find(2);
+        $recordingsParams = new GetRecordingsParameters();
+        $recordingsParams->setMeetingId($meeting->getAttendee()->getBbbMeetingId());
+        dump($this->bigBlueButton->getRecordings($recordingsParams));
     }
 }
