3e71a4546cd88170ffb11db3f43532188e6a626a
CodinKitchen <98607949+CodinKitchen@users.noreply.github.com>
2022-04-29 23:07:03 +0200

    Release/seo (#34)
    * improve html
    
    * optimize images
    
    * add sitemap

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
+    Presta\SitemapBundle\PrestaSitemapBundle::class => ['all' => true],
 ];
